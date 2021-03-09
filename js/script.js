00237489b289e9b30916bb6eac806af7cf71ac65
Filippo Garota <filogarota101@gmail.com>
2021-03-09 14:29:37 +0100

    Inizio progetto

---
+++ js/script.js
@@ -0,0 +1,18 @@
+/*Il computer deve generare 16 numeri casuali tra 1 e 100.
+I numeri non possono essere duplicati.
+In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
+L’utente non può inserire più volte lo stesso numero.
+Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
+La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
+Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.*/
+
+
+// creo un array che contiene le bombe
+var bombe = [];
+console.log(bombe);
+
+// genero i numeri casuali
+var randomNumber = bombe.push(random(1,10));
+function random(min, max) {
+  return Math.floor(Math.random()* (max - min + 1) + min);
+};
