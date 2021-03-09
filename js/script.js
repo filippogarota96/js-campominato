a6887ac4234de2e3b4c13ce27d343e8434c565a2
Filippo Garota <filogarota101@gmail.com>
2021-03-09 14:50:21 +0100

    Array di sedici numeri

--- js/script.js
+++ js/script.js
@@ -8,11 +8,14 @@ Al termine della partita il software deve comunicare il punteggio, cioè il nume
 
 
 // creo un array che contiene le bombe
-var bombe = [];
-console.log(bombe);
 
-// genero i numeri casuali
-var randomNumber = bombe.push(random(1,10));
+var bombe = [];
+
 function random(min, max) {
   return Math.floor(Math.random()* (max - min + 1) + min);
 };
+
+for ( i = 0; i <= 16 - 1; i++) {
+  var randomNumber = bombe.push(random(1,100));
+}
+console.log(bombe);
